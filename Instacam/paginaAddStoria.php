efb908359775d974fcaf34b46b245b2e7bdd18b7
Andreaprospo <proserpioandrea2006@gmail.com>
2024-12-21 19:08:35 +0100

    Inizio creazione pagine

--- Instacam/paginaAddStoria.php
+++ Instacam/paginaAddStoria.php
@@ -6,6 +6,16 @@
     <title>Document</title>
 </head>
     <body>
-        ADDSTORIA
+        <form action="gestoreAddStoria.php" method="POST">
+            <div>
+                <label for="file">Seleziona una foto:</label>
+                <input type="file" name="file" id="file" accept="image/*">
+            </div>
+            <div>
+                <label for="">data</label>
+                <input type="date" name="data">
+            </div>
+            <button>Manda</button>
+        </form>
     </body>
 </html>
