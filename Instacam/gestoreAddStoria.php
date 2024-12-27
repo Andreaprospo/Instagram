297fed2c6a2827efae35682d780ad34927bb8eb3
Andreaprospo <proserpioandrea2006@gmail.com>
2024-12-27 11:53:16 +0100

    Creazione pagina gestoreAddStoria
    Creazione pagina gestoreAddStoria e creazione di un esempio di file che si creerebbe quando si crea un nuovo profilo

--- Instacam/gestoreAddStoria.php
+++ Instacam/gestoreAddStoria.php
@@ -1,7 +1,62 @@
 <?php
 
-    print_r($_FILES);
-    print_r($_POST);
-    print_r($_GET);
+    if (isset($_FILES['file'])) 
+    {
 
+        //prendo info dal file
+        $fileTmpPath = $_FILES['file']['tmp_name'];
+        $fileName = $_FILES['file']['name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileType = $_FILES['file']['type'];
+      
+        $estensioneFile = explode(".", $fileName)[1];
+
+        //nome utente che prendo dalla sessione 
+        //$username = $_SESSION["utente"];
+        $username = "Marco";
+        $pathCartellaFoto = "FileUtenti/$username/Foto";
+
+        if (!is_dir($pathCartellaFoto)) {
+            mkdir($pathCartellaFoto, 0777, true); // Crea la directory se non esiste
+        }
+
+        //prendo l'ultimo id della storia così da identificarlo univocamente
+        $idStoria = getLastId($username) + 1;
+
+        //combino il nome utente e metto id 
+        $pathFinale = "$pathCartellaFoto/$idStoria.$estensioneFile";
+
+        if (move_uploaded_file($fileTmpPath, $pathFinale))
+            echo "Successo";
+        else
+            echo "Errore";
+
+    } 
+    else 
+    {
+        echo "No file caricati";
+    }
+
+?>
+
+<?php
+    function getLastId($username)
+    {
+        $pathPartenza = "FileUtenti/$username/Foto/";
+        $uploadDir = opendir($pathPartenza);
+        $lastId = 0;
+
+        while($nomeFileIntero = readdir($uploadDir))
+        {
+            $nomeFile = explode(".", $nomeFileIntero)[0];
+            if($nomeFile == "." || $nomeFile == "..")
+                continue;
+
+            if($lastId < $nomeFile)
+                $lastId = $nomeFile;
+            echo $nomeFile . "\n";
+        }
+        closedir($uploadDir);
+        return $lastId;
+    }
 ?>
--- Instacam/paginaAddStoria.php
+++ Instacam/paginaAddStoria.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Add Storia</title>
+    </head>
     <body>
-        <form action="gestoreAddStoria.php" method="POST">
+        <form action="gestoreAddStoria.php" method="post" enctype="multipart/form-data">
             <div>
                 <label for="file">Seleziona una foto:</label>
-                <input type="file" name="file" id="file" accept="image/*">
+                <input type="file" name="file" id="file" >
             </div>
             <div>
                 <label for="">data</label>
                 <input type="date" name="data">
             </div>
-            <button>Manda</button>
+            <button type="submit">Carica</button>
         </form>
     </body>
 </html>
